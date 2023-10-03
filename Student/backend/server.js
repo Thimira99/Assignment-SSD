fba18be67737fc62ec45d4362712d2d1eacb0aa0
unknown <thimiraamarakoon99@gmail.com>
2023-10-03 16:33:49 +0530

    added data validations on frontend and helmet on server

--- Student/backend/server.js
+++ Student/backend/server.js
@@ -1,37 +1,57 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const path = require('path');
-require('./db/db')
+const cors = require("cors");
+const path = require("path");
+const helmet = require("helmet");
+require("./db/db");
 // const uploads = require('./models/uploads')
 
-
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
-express.Router()
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "trusted-scripts.com"],
+      },
+    },
+    referrerPolicy: { policy: "same-origin" },
+    expectCt: { enforce: true, maxAge: 30 },
+    featurePolicy: {
+      features: {
+        fullscreen: ["'self'"],
+        geolocation: ["example.com"],
+      },
+    },
+    hsts: {
+      maxAge: 31536000,
+      includeSubDomains: true,
+      preload: true,
+    },
+    noSniff: true,
+  })
+);
+express.Router();
 
-const studentRoutes = require('./routes/stdRoutes');
-const authRoutes = require('./routes/auth');
-const apiRoutes = require('./routes/apiRoutes');
-const registerTopicRoutes = require('./routes/registerTopicRoutes');
+const studentRoutes = require("./routes/stdRoutes");
+const authRoutes = require("./routes/auth");
+const apiRoutes = require("./routes/apiRoutes");
+const registerTopicRoutes = require("./routes/registerTopicRoutes");
 
-const groupRoutes = require('./routes/groupRoutes');
+const groupRoutes = require("./routes/groupRoutes");
 
 const submissionApiRoutes = require("./routes/submissions");
 
 /**file upload*/
-const fileUpload = require('express-fileupload');
-const { patch } = require('./routes/submissions');
-
-
+const fileUpload = require("express-fileupload");
+const { patch } = require("./routes/submissions");
 
 //midlewares
 app.use(express.json());
 app.use(cors());
-
-
 
 app.use("/api/student", studentRoutes);
 app.use("/api/auth", authRoutes);
@@ -41,23 +61,21 @@ app.use("/api/topic", registerTopicRoutes);
 
 app.use("/api/student", studentRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/api", apiRoutes)
-app.use("/api/student", require('./routes/submissions'));
+app.use("/api", apiRoutes);
+app.use("/api/student", require("./routes/submissions"));
 
 app.use("/template", require("./routes/template"));
 
-
 /**file upload */
-app.use(fileUpload())
+app.use(fileUpload());
 // app.use("/uploads")
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const port = process.env.PORT || 8000;
-var server = 
-app.listen(port, () => {
-    console.log(`Listning on port ${port}`)
+var server = app.listen(port, () => {
+  console.log(`Listning on port ${port}`);
 });
 
 //Ishani
-module.exports = server
+module.exports = server;
--- Student/frontend/src/components/pages/Group/Group.js
+++ Student/frontend/src/components/pages/Group/Group.js
@@ -1,185 +1,235 @@
-import React, { Component, useEffect, useState } from 'react';
-import Header from '../../header/header';
-import Sidebar from '../../sidebar/Sidebar';
-import profile from '../../images/profile.png';
-import { Card, Body, Img, Button, Title, Text, Form } from 'react-bootstrap';
+import React, { Component, useEffect, useState } from "react";
+import Header from "../../header/header";
+import Sidebar from "../../sidebar/Sidebar";
+import profile from "../../images/profile.png";
+import { Card, Body, Img, Button, Title, Text, Form } from "react-bootstrap";
 
-import groupStyles from './Group.module.css';
-import axios from 'axios';
-import { useHistory } from 'react-router-dom';
+import groupStyles from "./Group.module.css";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 function Group() {
+  const history = useHistory();
+  const id = localStorage.getItem("id");
+  const [groupName, setGroupName] = useState("");
+  const [groupLeaderName, setgroupLeaderName] = useState("");
+  const [groupLeaderId, setgroupLeaderId] = useState("");
+  const [memberTwoName, setmemberTwoName] = useState("");
+  const [memberTwoId, setmemberTwoId] = useState("");
+  const [memberThreeName, setmemberThreeName] = useState("");
+  const [memberThreeId, setmemberThreeId] = useState("");
+  const [memberFourName, setmemberFourName] = useState("");
+  const [memberFourId, setmemberFourId] = useState("");
 
-    const history = useHistory();
-    const id = localStorage.getItem('id');
-    const [groupName, setGroupName] = useState("");
-    const [groupLeaderName, setgroupLeaderName] = useState("");
-    const [groupLeaderId, setgroupLeaderId] = useState("");
-    const [memberTwoName, setmemberTwoName] = useState("");
-    const [memberTwoId, setmemberTwoId] = useState("");
-    const [memberThreeName, setmemberThreeName] = useState("");
-    const [memberThreeId, setmemberThreeId] = useState("");
-    const [memberFourName, setmemberFourName] = useState("");
-    const [memberFourId, setmemberFourId] = useState("");
+  function onSubmit(e) {
+    e.preventDefault();
 
+    //----------------------------------------------------------------------
+    // Check for potentially harmful content using regular expressions
+    const harmfulContentRegex =
+      /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/i;
 
-    function onSubmit(e) {
-        e.preventDefault();
-
-        const newGroup = {
-            groupName,
-            groupLeaderName,
-            groupLeaderId,
-            memberTwoName,
-            memberTwoId,
-            memberThreeName,
-            memberThreeId,
-            memberFourName,
-            memberFourId
-        }
-
-        axios.post("http://localhost:8000/api/group/post", newGroup).then((res) => {
-            alert("Group Created")
-            localStorage.setItem('groupId', res.data.student._id);
-            localStorage.setItem('groupLeaderId', res.data.student.groupLeaderId);
-            history.push('/viewGroups');
-        }).catch((error) => {
-            alert(error);
-        })
+    // Check if any of the input fields contain harmful content
+    if (
+      harmfulContentRegex.test(groupName) ||
+      harmfulContentRegex.test(groupLeaderName) ||
+      harmfulContentRegex.test(groupLeaderId) ||
+      harmfulContentRegex.test(memberTwoName) ||
+      harmfulContentRegex.test(memberTwoId) ||
+      harmfulContentRegex.test(memberThreeName) ||
+      harmfulContentRegex.test(memberThreeId) ||
+      harmfulContentRegex.test(memberFourName) ||
+      harmfulContentRegex.test(memberFourId)
+    ) {
+      // Display an alert if harmful content is detected
+      alert(
+        "Potentially harmful content detected. Please remove any script tags or suspicious content."
+      );
+      return;
     }
 
-    return (
-        <div className='main-wrapper'>
-            <div className='app-header'>
-                <Header />
-            </div>
-            <div className='app-body'>
-                <div className='body-wrapper'>
-                    <div className='app-sidebar'>
-                        <Sidebar />
+    // If no harmful content is detected, proceed with form submission
+    const newGroup = {
+      groupName,
+      groupLeaderName,
+      groupLeaderId,
+      memberTwoName,
+      memberTwoId,
+      memberThreeName,
+      memberThreeId,
+      memberFourName,
+      memberFourId,
+    };
+    //----------------------------------------------------------------------
+
+    //  const newGroup = {
+    //    groupName,
+    //    groupLeaderName,
+    //    groupLeaderId,
+    //    memberTwoName,
+    //    memberTwoId,
+    //    memberThreeName,
+    //    memberThreeId,
+    //    memberFourName,
+    //    memberFourId,
+    //  };
+
+    console.log(newGroup);
+
+    axios
+      .post("http://localhost:8000/api/group/post", newGroup)
+      .then((res) => {
+        alert("Group Created");
+        localStorage.setItem("groupId", res.data.student._id);
+        localStorage.setItem("groupLeaderId", res.data.student.groupLeaderId);
+        history.push("/viewGroups");
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }
+
+  return (
+    <div className="main-wrapper">
+      <div className="app-header">
+        <Header />
+      </div>
+      <div className="app-body">
+        <div className="body-wrapper">
+          <div className="app-sidebar">
+            <Sidebar />
+          </div>
+          <div className="app-content">
+            <div className={groupStyles.header}>
+              <h1>Create Group</h1>
+              <a href="http://localhost:8000" target="_blank">
+                Visit Site
+              </a>
+              <div className={groupStyles.mainForm}>
+                <form onSubmit={onSubmit}>
+                  <div className="form-group">
+                    <h4>Only Leader should be fill this form</h4>
+                    <label for="groupName">Group Name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter Group Name"
+                      onChange={(e) => {
+                        setGroupName(e.target.value);
+                      }}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="groupLeaderName">Group Leader Name:</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Group Leader Name"
+                      onChange={(e) => {
+                        setgroupLeaderName(e.target.value);
+                      }}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="groupLeaderId">Group Leader Id</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Group Leader Id"
+                      onChange={(e) => {
+                        setgroupLeaderId(e.target.value);
+                      }}
+                    />
+                  </div>
+                  <div className={groupStyles.membersDetail}>
+                    <h3>Group Members</h3>
+                    <div className={groupStyles.membersDetails}>
+                      <div className="form-group">
+                        <label for="memberTwoName">Member 2(Name):</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Member 2(Name)"
+                          onChange={(e) => {
+                            setmemberTwoName(e.target.value);
+                          }}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label for="memberTwoId">Member 2(Id):</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Member 2(Id)"
+                          onChange={(e) => {
+                            setmemberTwoId(e.target.value);
+                          }}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label for="memberThreeName">Member 3(Name):</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Member 3(Name)"
+                          onChange={(e) => {
+                            setmemberThreeName(e.target.value);
+                          }}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label for="memberThreeId">Member 3(Id):</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Member 3(Id)"
+                          onChange={(e) => {
+                            setmemberThreeId(e.target.value);
+                          }}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label for="memberFourName">Member 4(Name):</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Member 4(Name)"
+                          onChange={(e) => {
+                            setmemberFourName(e.target.value);
+                          }}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label for="memberFourId">Member 4(Id):</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Member 4(Id)"
+                          onChange={(e) => {
+                            setmemberFourId(e.target.value);
+                          }}
+                        />
+                      </div>
                     </div>
-                    <div className='app-content'>
-                        <div className={groupStyles.header}>
-                            <h1>Create Group</h1>
-                            <div className={groupStyles.mainForm}>
-                                <form onSubmit={onSubmit}>
-                                    <div className="form-group">
-                                        <h4>Only Leader should be fill this form</h4>
-                                        <label for="groupName">Group Name</label>
-                                        <input
-                                            type="text"
-                                            className="form-control"
-                                            placeholder="Enter Group Name"
-                                            onChange={(e) => {
-                                                setGroupName(e.target.value);
-                                            }} />
-                                    </div>
-                                    <div className="form-group">
-                                        <label for="groupLeaderName">Group Leader Name:</label>
-                                        <input
-                                            type="text"
-                                            className="form-control"
-                                            placeholder="Group Leader Name"
-                                            onChange={(e) => {
-                                                setgroupLeaderName(e.target.value);
-                                            }} />
-                                    </div>
-                                    <div className="form-group">
-                                        <label for="groupLeaderId">Group Leader Id</label>
-                                        <input
-                                            type="text"
-                                            className="form-control"
-                                            placeholder="Group Leader Id"
-                                            onChange={(e) => {
-                                                setgroupLeaderId(e.target.value);
-                                            }} />
-                                    </div>
-                                    <div className={groupStyles.membersDetail}>
-                                        <h3>Group Members</h3>
-                                        <div className={groupStyles.membersDetails}>
-                                            <div className="form-group">
-                                                <label for="memberTwoName">Member 2(Name):</label>
-                                                <input
-                                                    type="text"
-                                                    className="form-control"
-                                                    placeholder="Member 2(Name)"
-                                                    onChange={(e) => {
-                                                        setmemberTwoName(e.target.value);
-                                                    }} />
-                                            </div>
-                                            <div className="form-group">
-                                                <label for="memberTwoId">Member 2(Id):</label>
-                                                <input
-                                                    type="text"
-                                                    className="form-control"
-                                                    placeholder="Member 2(Id)"
-                                                    onChange={(e) => {
-                                                        setmemberTwoId(e.target.value);
-                                                    }} />
-                                            </div>
-                                            <div className="form-group">
-                                                <label for="memberThreeName">Member 3(Name):</label>
-                                                <input
-                                                    type="text"
-                                                    className="form-control"
-                                                    placeholder="Member 3(Name)"
-                                                    onChange={(e) => {
-                                                        setmemberThreeName(e.target.value);
-                                                    }} />
-                                            </div>
-                                            <div className="form-group">
-                                                <label for="memberThreeId">Member 3(Id):</label>
-                                                <input
-                                                    type="text"
-                                                    className="form-control"
-                                                    placeholder="Member 3(Id)"
-                                                    onChange={(e) => {
-                                                        setmemberThreeId(e.target.value);
-                                                    }} />
-                                            </div>
-                                            <div className="form-group">
-                                                <label for="memberFourName">Member 4(Name):</label>
-                                                <input
-                                                    type="text"
-                                                    className="form-control"
-                                                    placeholder="Member 4(Name)"
-                                                    onChange={(e) => {
-                                                        setmemberFourName(e.target.value);
-                                                    }} />
-                                            </div>
-                                            <div className="form-group">
-                                                <label for="memberFourId">Member 4(Id):</label>
-                                                <input
-                                                    type="text"
-                                                    className="form-control"
-                                                    placeholder="Member 4(Id)"
-                                                    onChange={(e) => {
-                                                        setmemberFourId(e.target.value);
-                                                    }} />
-                                            </div>
-                                        </div>
-
-
-                                    </div>
-                                    <div className={groupStyles.submitButton}>
-                                        <button type="submit" className="btn btn-primary">Submit</button>
-                                    </div>
-
-                                </form>
-
-                            </div>
-                            <div className={groupStyles.groups}>
-                                <a href='/viewGroups' className='btn btn-primary'>View Registered Groups</a>
-                            </div>
-
-                        </div>
-
-
-                    </div>
-                </div>
+                  </div>
+                  <div className={groupStyles.submitButton}>
+                    <button type="submit" className="btn btn-primary">
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </div>
+              <div className={groupStyles.groups}>
+                <a href="/viewGroups" className="btn btn-primary">
+                  View Registered Groups
+                </a>
+              </div>
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Group;
